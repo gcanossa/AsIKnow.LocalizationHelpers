b847da3af93a0d472ba92ce6b5352cff553054a8
goldenass <gianmaria.canossa@gmail.com>
2018-01-10 09:42:31 +0100

    first commit

---
+++ src/AsIKnow.LocalizationHelpers/IApplicationBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace AsIKnow.LocalizationHelpers
+{
+    public static class IApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseLocalizationHelpers(this IApplicationBuilder ext, LocalizationOptions options)
+        {
+            CultureInfo[] supportedCultures = options.SupportedCultures.Select(p => new CultureInfo(p)).ToArray();
+
+            ext.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(options.DefaultCulture),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+
+            return ext;
+        }
+    }
+}
---
+++ src/AsIKnow.LocalizationHelpers/LocalizationOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace AsIKnow.LocalizationHelpers
+{
+    public class LocalizationOptions
+    {
+        public string DefaultCulture { get; set; }
+        public string[] SupportedCultures { get; set; }
+    }
+}
